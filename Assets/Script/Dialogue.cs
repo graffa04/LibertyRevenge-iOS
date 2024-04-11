b10b4c46c62491f25ae7feb91ab8ba38dfb8e263
Raffaele Marino <raffaelemarino04@gmail.com>
2024-04-11 19:28:16 +0200

    Tutorial scene completed

--- Assets/Script/Dialogue.cs
+++ Assets/Script/Dialogue.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Dialogue : MonoBehaviour
 {
+    public float doubleClickThreshold = 0.5f; 
+    private float lastClickTime = 0f;
     public TextMeshProUGUI TextComponent; 
     public string[] lines;
     public float textSpeed;
     private int index;
     public GameObject actionButton;
     public GameObject PlayerFire;
+    public GameObject PlayerShield;
     public GameObject Panel;
     public GameObject Panel2;
+    public GameObject Panel3;
+    public GameObject JoeBiden;
+    public GameObject Balloon;
+    public GameObject JoeBiden1;
+    public GameObject Balloon1;
+    public GameObject PlayGame;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,17 +34,27 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
+    {
+        if (Time.time - lastClickTime < doubleClickThreshold)
         {
-            if(TextComponent.text == lines[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                TextComponent.text = lines[index];
-            }
+            OnDoubleClick();
+        }
+
+        lastClickTime = Time.time;
+    }
+    }
+
+    void OnDoubleClick()
+    {
+        if(TextComponent.text == lines[index])
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            TextComponent.text = lines[index];
         }
     }
 
@@ -61,9 +81,19 @@ public class Dialogue : MonoBehaviour
             TextComponent.text = string.Empty;
             StartCoroutine(TypeLine());
 
-            if(index == 4)
+            if(index == 5)
             {
-                DoAction(); 
+                Fire(); 
+            } 
+            
+            if(index == 6)
+            {
+                Shield();
+            } 
+
+            if(index == 8)
+            {
+                PlayGame.gameObject.SetActive(true);
             }
         }
         else 
@@ -71,18 +101,51 @@ public class Dialogue : MonoBehaviour
             gameObject.SetActive(false);            
         }
     }
-    public void DoAction()
+    public void Fire()
     {
         
         if (Panel != null)
         {
-            Debug.Log("Azione eseguita!");
-
             Destroy(Panel.gameObject);
 
             Panel2.gameObject.SetActive(true);
 
             PlayerFire.gameObject.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("Il riferimento al pannello è nullo.");
+        }
+    }
+
+    public void Shield()
+    {
+        if (Panel2 != null)
+        {
+            Destroy(Panel2.gameObject);
+            
+            Destroy(PlayerFire.gameObject);
+
+            Panel3.gameObject.SetActive(true);
+
+            // Puoi utilizzare anche CanvasGroup se gli oggetti sono UI
+            CanvasGroup joeBidenCanvasGroup = JoeBiden1.GetComponent<CanvasGroup>();
+            if (joeBidenCanvasGroup != null)
+            {
+                joeBidenCanvasGroup.alpha = 1f; // Imposta l'opacità desiderata (0 = trasparente, 1 = opaco)
+            }
+
+            CanvasGroup balloonCanvasGroup = Balloon1.GetComponent<CanvasGroup>();
+            if (balloonCanvasGroup != null)
+            {
+                balloonCanvasGroup.alpha = 1f; // Imposta l'opacità desiderata (0 = trasparente, 1 = opaco)
+            }
+
+            Destroy(JoeBiden.gameObject);
+
+            Destroy(Balloon.gameObject);
+
+            PlayerShield.gameObject.SetActive(true);
         }
         else
         {
